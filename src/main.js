7f38feb19ac12740fd7110cb21772a33e1cbd823
nospame <mapson.evan@gmail.com>
2022-05-25 10:58:07 -0700

    install uikit, build initial buttons on page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+UIkit.use(Icons);
 
 createApp(App).use(router).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,52 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      entry: '',
+      result: ''
+    }
+  },
+  methods: {
+    calculate: function () {
+      this.result = this.entry.split(/\D/)
+    },
+    multiply: () => {
+
+    },
+    divide: () => {
+
+    },
+    add: () => {
+
+    },
+    subtract: () => {
+
+    }
   }
 }
 </script>
+
+<template>
+  <div class="home">
+    <p>{{ result }}</p>
+    <input v-model="entry" type="text">
+    <br />
+    <button class="uk-button uk-button-default">1</button>
+    <button class="uk-button uk-button-default">2</button>
+    <button class="uk-button uk-button-default">3</button>
+    <br />
+    <button class="uk-button uk-button-default">4</button>
+    <button class="uk-button uk-button-default">5</button>
+    <button class="uk-button uk-button-default">6</button>
+    <br />
+    <button class="uk-button uk-button-default">7</button>
+    <button class="uk-button uk-button-default">8</button>
+    <button class="uk-button uk-button-default">9</button>
+    <br />
+    <button class="uk-button uk-button-default">0</button>
+    <button v-on:click="calculate()" class="uk-button uk-button-default">Enter</button>
+  </div>
+</template>
+
